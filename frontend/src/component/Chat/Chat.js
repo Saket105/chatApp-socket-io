dbaa16b6261f8fed483e9a813267ea1be0951643
saket105 <saketcool35@gmail.com>
2022-04-24 11:45:24 +0530

    Error in chat.js

--- frontend/src/component/Chat/Chat.js
+++ frontend/src/component/Chat/Chat.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { user } from "../Join/Join";
+import "./Chat.css";
+import socketIo from "socket.io-client";
+import sendLogo from "../../images/send.png";
+
+const ENDPOINT = "http://localhost:4500/";
 
 const Chat = () => {
+  useEffect(() => {
+    const socket = socketIo(ENDPOINT, { transports: ["websocket"] });
+    socket.on("connect", () => {
+      alert("Connected!");
+    });
+    socket.emit("joined", { user });
+
+    socket.on("welcome", (data) => {
+      console.log(data.user, data.message);
+    });
+
+    socket.on("userJoined", (data) => {
+      console.log(data.user, data.message);
+    });
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome {user}</h1>
+    <div className="chatPage">
+      <div className="chatContainer">
+        <div className="header"></div>
+        <div className="chatBox"></div>
+        <div className="inputBox">
+          <input type="text" id="chatInput" />
+          <button className="sendBtn">
+            <img src={sendLogo} alt="sendImage" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const socketIO = require("socket.io");
 
 const app = express();
 const port = 4500 || process.env.PORT;
+const users = [{}];
 
 app.use(cors());
 app.get("/", (req, res) => {
@@ -15,8 +16,25 @@ const server = https.createServer(app);
 
 const io = socketIO(server);
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New Connection");
+
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined`);
+    socket.broadcast.emit("userJoined", {
+      user: "Admin",
+      message: `${users[socket.id]}  has joined`,
+    });
+    socket.emit("welcome", {
+      user: "Admin",
+      message: `Welcome to the chat ${users[socket.id]}`,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Left");
+  });
 });
 
 server.listen(port, () => {
